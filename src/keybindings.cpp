70cc5cae8cdd6487f9f0a780985e1c5449b24f9b
Marc Aldorasi <m101010a@gmail.com>
2022-11-20 22:49:50 -0500

    Add PageUp and PageDown keybinds

--- src/keybindings.cpp
+++ src/keybindings.cpp
@@ -67,21 +67,23 @@ struct JsBindingData
 /* Common */
 static const KbBindingData defaultKbBindings[] =
 {
-	{ SDL_SCANCODE_LEFT,   Input::Left  },
-	{ SDL_SCANCODE_RIGHT,  Input::Right },
-	{ SDL_SCANCODE_UP,     Input::Up    },
-	{ SDL_SCANCODE_DOWN,   Input::Down  },
-	{ SDL_SCANCODE_SPACE,  Input::C     },
-	{ SDL_SCANCODE_RETURN, Input::C     },
-	{ SDL_SCANCODE_ESCAPE, Input::B     },
-	{ SDL_SCANCODE_KP_0,   Input::B     },
-	{ SDL_SCANCODE_LSHIFT, Input::A     },
-	{ SDL_SCANCODE_X,      Input::B     },
-	{ SDL_SCANCODE_D,      Input::Z     },
-	{ SDL_SCANCODE_Q,      Input::L     },
-	{ SDL_SCANCODE_W,      Input::R     },
-	{ SDL_SCANCODE_A,      Input::X     },
-	{ SDL_SCANCODE_S,      Input::Y     }
+	{ SDL_SCANCODE_LEFT,     Input::Left  },
+	{ SDL_SCANCODE_RIGHT,    Input::Right },
+	{ SDL_SCANCODE_UP,       Input::Up    },
+	{ SDL_SCANCODE_DOWN,     Input::Down  },
+	{ SDL_SCANCODE_LSHIFT,   Input::A     },
+	{ SDL_SCANCODE_ESCAPE,   Input::B     },
+	{ SDL_SCANCODE_KP_0,     Input::B     },
+	{ SDL_SCANCODE_X,        Input::B     },
+	{ SDL_SCANCODE_RETURN,   Input::C     },
+	{ SDL_SCANCODE_SPACE,    Input::C     },
+	{ SDL_SCANCODE_Q,        Input::L     },
+	{ SDL_SCANCODE_PAGEUP,   Input::L     },
+	{ SDL_SCANCODE_W,        Input::R     },
+	{ SDL_SCANCODE_PAGEDOWN, Input::R     },
+	{ SDL_SCANCODE_A,        Input::X     },
+	{ SDL_SCANCODE_S,        Input::Y     },
+	{ SDL_SCANCODE_D,        Input::Z     },
 };
 
 /* RGSS1 */
